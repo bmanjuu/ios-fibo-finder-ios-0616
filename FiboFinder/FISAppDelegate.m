c9eb6bed167e8a3cf023ead83b5be10d2d4f5e19
bmanjuu <bettyofung@gmail.com>
2016-06-02 10:54:32 -0400

    third pass at fibonacci finder

--- FiboFinder/FISAppDelegate.m
+++ FiboFinder/FISAppDelegate.m
@@ -9,10 +9,25 @@
     return YES;
 }
 
-/**
- 
- * Write your method implemenation here.
- 
- */
+-(NSArray *)arrayWithFibonacciSequenceToIndex:(NSUInteger)index{
+    
+    NSMutableArray *sequence = [[NSMutableArray alloc] init];
+    
+    for(NSUInteger i = 0; i < index + 1; i++){
+        if(i == 0){
+            [sequence addObject:@0];
+        }
+        else if (i == 1){
+            [sequence addObject:@1];
+        }
+        else {
+            NSInteger lastNumber = [sequence[i-1] integerValue];
+            NSInteger secondToLastNumber = [sequence[i-2] integerValue];
+            NSNumber *nextFiboNumber = @(lastNumber+secondToLastNumber);
+            [sequence addObject:nextFiboNumber];
+        }
+    }
+    return sequence;
+}
 
 @end
